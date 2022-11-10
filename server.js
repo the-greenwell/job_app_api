6aee623ad0a89924e18b5de3e79357c14206a452
the-greenwell <ant.greenwell2@gmail.com>
2022-11-09 16:31:53 -0800

    gitignore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config();
 require('./config/mongoose.config');
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const routes = require('./routes/jobs.routes');
